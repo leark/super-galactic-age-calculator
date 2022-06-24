32c942d4d4a52b111b2b5b365cd798a23d8f8edd
Seung Lee <shl7@uw.edu>
2022-06-24 11:16:50 -0700

    Pass test for ageInMercuryYears()

--- src/js/ageCalculator.js
+++ src/js/ageCalculator.js
@@ -1,9 +1,14 @@
 export default class AgeCalculator {
   constructor(ageInEarthYears) {
     this.ageInEarthYears = ageInEarthYears;
+    this.earthDaysInMercuryYear = 88;
   }
 
   ageInEarthDays() {
     return this.ageInEarthYears * 365;
+  }
+
+  ageInMercuryYears() {
+    return Math.round(this.ageInEarthDays() / this.earthDaysInMercuryYear);
   }
 }
